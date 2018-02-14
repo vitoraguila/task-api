683a2eed09d5b8c7e49da9cddb401d7c8abf3b0e
Vitor Hugo Aguila Oliveira <vitor.aguila1@gmail.com>
2018-02-14 17:32:23 -0200

    adding shoulda matchers for users spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value("vitoraguila@hotmail.com").for(:email) }
+
+
+
 end
